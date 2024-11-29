39102d99d730e63871e2d1a8c94179655edae3d8
Bruno Souza Lima <134153119+SouzaBrun0@users.noreply.github.com>
2024-11-28 23:30:12 -0300

    5

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,10 @@
 import axios from 'axios';
 
-// Configuração da API Unsplash
 const API_KEY = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
 const BASE_URL = 'https://api.unsplash.com/search/photos';
 
-// Configuração da API do Backend
-const API_BASE_URL = 'http://localhost:5000'; // URL do backend
+const API_BASE_URL = 'http://localhost:5000'; 
 
-// Função para buscar imagens da API Unsplash
 export const fetchImages = async (query: string) => {
   try {
     const response = await axios.get(BASE_URL, {
@@ -16,29 +13,27 @@ export const fetchImages = async (query: string) => {
       },
       params: {
         query: query,
-        per_page: 20, // Limita a 20 imagens por consulta
+        per_page: 20, 
       },
     });
 
-    return response.data.results; // Retorna os resultados das imagens
+    return response.data.results; 
   } catch (error) {
     console.error('Erro ao buscar imagens:', error);
     throw error;
   }
 };
 
-// Função para buscar favoritos do backend
 export const fetchFavorites = async () => {
   try {
     const response = await axios.get(`${API_BASE_URL}/favorites`);
-    return response.data; // Retorna a lista de favoritos
+    return response.data; 
   } catch (error) {
     console.error('Erro ao buscar favoritos:', error);
     throw error;
   }
 };
 
-// Função para adicionar uma imagem como favorita no backend
 export const addFavorite = async (image: any) => {
   try {
     await axios.post(`${API_BASE_URL}/favorites`, image);
@@ -48,7 +43,6 @@ export const addFavorite = async (image: any) => {
   }
 };
 
-// Função para remover um favorito do backend
 export const removeFavorite = async (id: string) => {
   try {
     await axios.delete(`${API_BASE_URL}/favorites/${id}`);
